8ee2183ff9da49cea7b66d6b42d88af91be7e49c
evi1haxor <architdwivedi.off@gmail.com>
2019-05-23 16:36:03 +0530

    added tweaks in navigation bar

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ $(function() {
 
     $(window).scroll(function() {
   
-      var mass = Math.max(45, 80-0.1*$(this).scrollTop()) + 'px';
+      var mass = Math.max(3, 5-0.009*$(this).scrollTop()) + 'rem';
       var pad = Math.max(10, 16-0.02*$(this).scrollTop()) + 'px';
       $('.el4').css({'font-size': mass, 'line-height': mass, 'padding': pad});
     });
