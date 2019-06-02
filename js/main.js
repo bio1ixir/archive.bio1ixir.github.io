c435cd5382aaedd30b5b9fb73db7033e97c37be3
evi1haxor <architdwivedi.off@gmail.com>
2019-06-02 19:09:17 +0530

    modiied JS (and lil part of CSS) that resolves #13

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 $(function() {
 
   $(window).scroll(function() {
-    if ($(window).width() <= 776) {
+    if ($(window).width() < 754) {
       var mass = 100 + '%';
       if ($(window).scrollTop() > 0) {
         $('.container').css({
@@ -53,28 +53,80 @@ $(function() {
           'transition-property': 'all',
           'transition-duration': '1s',
           'transition-timing-function': 'ease',
-          'transition-delay': '0s',
-          'align-content': 'center'}
+          'transition-delay': '0s'}
         );
+      }
+    }
+    //else {
+      //$('.nav-s').css(
+      //  {'display': 'none'}
+     // );
+    //}
+
+
+    else if ($(window).width() <= 776 && $(window).width() >= 754) {
+      var mass = 100 + '%';
+      if ($(window).scrollTop() > 0) {
+        $('.container').css({
+        'width': mass,
+        'transition-property': 'all',
+        'transition-duration': '0.5s',
+        'transition-timing-function': 'ease',
+        'transition-delay': '0s'});
+
+        $('.nav-s').css(
+          {'display': 'none'}
+        );
+
+        $('.el4').css({
+        'font-size': '2rem', 
+        'line-height': '0rem', 
+        'padding': '30px 0% 0px 0%',
+        'transition-property': 'all',
+        'transition-duration': '1s',
+        'transition-timing-function': 'ease',
+        'transition-delay': '0s'});
+      }
+      else {
+        mass = 90 + '%';
+        $('.container').css({'width': mass,
+        'transition-property': 'all',
+        'transition-duration': '0.5s',
+        'transition-timing-function': 'ease',
+        'transition-delay': '0s'});
+
+        $('.el4').css({'font-size': '3rem', 
+        'line-height': '0rem', 
+        'padding': '30px 0px 0px 0px',
+        'transition-property': 'all',
+        'transition-duration': '1s',
+        'transition-timing-function': 'ease',
+        'transition-delay': '0s'});
       }
     }
 
 
-
-    if ($(window).width() < 1076 && $(window).width() > 776) {
+    else if ($(window).width() <= 1076 && $(window).width() > 776) {
       var mass = Math.max(2, 4-0.18*$(this).scrollTop()) + 'rem';
       var pad = Math.max(13, 16-0.05*$(this).scrollTop()) + 'px';
       $('.el4').css({'font-size': mass, 'line-height': mass, 'padding': pad});
+
+      $('.container').css({'width': '93%'});
     }
-   else if ($(window).width() < 1586  && $(window).width() >= 1076) {
+   else if ($(window).width() <= 1586  && $(window).width() > 1076) {
       var mass = Math.max(2, 4-0.02*$(this).scrollTop()) + 'rem';
       var pad = Math.max(5, 16-0.02*$(this).scrollTop()) + 'px';
       $('.el4').css({'font-size': mass, 'line-height': mass, 'padding': pad});
+
+      $('.container').css({'width': '85%'});
     }
-    else if ($(window).width() >= 1586) {
+    else if ($(window).width() > 1586) {
       var mass = Math.max(3, 4.5-0.02*$(this).scrollTop()) + 'rem';
       var pad = Math.max(10, 16-0.02*$(this).scrollTop()) + 'px';
       $('.el4').css({'font-size': mass, 'line-height': mass, 'padding': pad});
+
+      $('.container').css({'width': '85rem'});
+
     }
   });
 });
