4e9fa078e4d4cd1fd37e2e31712c8f9abdcbd191
evi1haxor <architdwivedi.off@gmail.com>
2020-05-23 23:33:53 +0530

    flossed and cleaned the js

--- js/main.js
+++ js/main.js
@@ -1,18 +1,19 @@
 $(function() {
 
-  $(window).scroll(function() {
-    if ($(window).width() <= 776) {
+$(window).scroll(function(){
+    if ($(window).width() <= 776){
       var mass = 100 + '%';
+
       if ($(window).scrollTop() > 0.5) {
         $('.container').css({
-        'width': mass,
-        'padding':'1rem 1.5rem',
-        'transition-property': 'all',
-        'transition-duration': '0.1s',
-        'transition-timing-function': 'ease',
-        'transition-delay': '0s'});
-        
-        
+          'width': mass,
+          'padding':'1rem 1.5rem',
+          'transition-property': 'all',
+          'transition-duration': '0.1s',
+          'transition-timing-function': 'ease',
+          'transition-delay': '0s'});
+
+
         $('.contact').css({
           'width': mass,
           'padding': '3rem 30px',
@@ -20,54 +21,52 @@ $(function() {
           'transition-duration': '0.1s',
           'transition-timing-function': 'ease',
           'transition-delay': '0s'});
-  
 
-          if ($(window).width() <= 600) {
-            $('.el4').css({
-              'font-size': '1.8rem', 
-              'line-height': '0rem', 
-              'padding': '0px 0% 0px 0%',
-              'transition-property': 'all',
-              'transition-duration': '0.5s',
-              'transition-timing-function': 'ease',
-              'transition-delay': '0s'});
-      
-          }
-          else {
-        $('.el4').css({
-        'font-size': '2.4rem', 
-        'line-height': '0rem', 
-        'padding': '0px 0% 0px 0%',
-        'transition-property': 'all',
-        'transition-duration': '0.5s',
-        'transition-timing-function': 'ease',
-        'transition-delay': '0s'});
+
+        if ($(window).width() <= 600) {
+          $('.el4').css({
+            'font-size': '1.8rem', 
+            'line-height': '0rem', 
+            'padding': '0px 0% 0px 0%',
+            'transition-property': 'all',
+            'transition-duration': '0.5s',
+            'transition-timing-function': 'ease',
+            'transition-delay': '0s'});
         }
-        $('.nav-s').css(
-          {'display': 'grid',
+
+        else {
+          $('.el4').css({
+            'font-size': '2.4rem', 
+            'line-height': '0rem', 
+            'padding': '0px 0% 0px 0%',
+            'transition-property': 'all',
+            'transition-duration': '0.5s',
+            'transition-timing-function': 'ease',
+            'transition-delay': '0s'});
+        }
+
+        $('.nav-s').css({
+          'display': 'grid',
           'grid-template-columns': 'auto auto auto',
           'transition-property': 'all',
           'transition-duration': '0.1s',
           'transition-timing-function': 'ease',
           'transition-delay': '0s',
-          'align-content': 'center'}
-        );
+          'align-content': 'center'});
 
-        $('.nav-left').css(
-          {'display': 'none'}
-        );
+        $('.nav-left').css({
+          'display': 'none'});
 
         $('.nav-right').css(
-          {'display': 'none'}
-        );
+        {'display': 'none'});
       }
-      else {
+
+      else{
         mass = 90 + '%';
         $('.container').css({'width': mass,
         'padding':'1rem 1.5rem',
         'transition-property': 'all',
         'transition-duration': '0.1s',
-        'padding': '1.4rem 1.5rem',
         'transition-timing-function': 'ease',
         'transition-delay': '0s'});
 
@@ -79,113 +78,113 @@ $(function() {
         'transition-delay': '0s'});
 
         $('.el4').css({
-          'font-size': '2rem', 
-          'line-height': '0rem', 
-          'padding': '0px 0% 0px 0%',
-          'transition-property': 'all',
-          'transition-duration': '0.5s',
-          'transition-timing-function': 'ease',
-          'transition-delay': '0s'});
-  
+        'font-size': '2rem', 
+        'line-height': '0rem', 
+        'padding': '0px 0% 0px 0%',
+        'transition-property': 'all',
+        'transition-duration': '0.5s',
+        'transition-timing-function': 'ease',
+        'transition-delay': '0s'});
+
         $('.nav-s').css(
-          {'display': 'grid',
-          'grid-template-columns': 'auto auto auto',
-          'transition-property': 'all',
-          'transition-duration': '0.1s',
-          'transition-timing-function': 'ease',
-          'transition-delay': '0s',
-          'align-content': 'center'}
+        {'display': 'grid',
+        'grid-template-columns': 'auto auto auto',
+        'transition-property': 'all',
+        'transition-duration': '0.1s',
+        'transition-timing-function': 'ease',
+        'transition-delay': '0s',
+        'align-content': 'center'}
         );
 
-
         $('.nav-left').css(
-          {'display': 'none'}
+        {'display': 'none'}
         );
 
         $('.nav-right').css(
-          {'display': 'none'}
+        {'display': 'none'}
         );
       }
     }
 
-    if ($(window).width() <= 1076 && $(window).width() > 776) {
-      if ($(window).scrollTop() > 0.5) {
+
+
+    if ($(window).width() <= 1076 && $(window).width() > 776){
+      if ($(window).scrollTop() > 0.5){
         $('.el4').css({'font-size': '2rem', 'line-height': '2rem', 'padding': '13px'});
       }
+
       else {
         $('.el4').css({'font-size': '3rem', 'line-height': '3rem', 'padding': '15px'});
       }
-      $('.container').css({'width': '93%'});
 
-      $('.contact').css({'width': '93%'});
+      $('.container').css({
+        'width': '93%'});
 
-      $('.nav-s').css(
-        {'display': 'none'}
-      );
+      $('.contact').css({
+        'width': '93%'});
 
-      $('.nav-left').css(
-        {'display': 'grid'}
-      );
-    
-    
-      $('.nav-right').css(
-        {'display': 'grid'}
-      );
-    }
-   if ($(window).width() <= 1586  && $(window).width() > 1076) {
-    if ($(window).scrollTop() > 0.5) {
-      $('.el4').css({'font-size': '2.2rem', 'line-height': '2.2rem', 'padding': '5px'});
-    }
-    else {
-      $('.el4').css({'font-size': '3.3rem', 'line-height': '3.3rem', 'padding': '16px'});
+      $('.nav-s').css({
+        'display': 'none'});
+
+      $('.nav-left').css({
+        'display': 'grid'});
+
+      $('.nav-right').css({
+        'display': 'grid'});
     }
 
-      $('.container').css({'width': '85%'});
 
-      $('.contact').css({'width': '85%'});
+    if ($(window).width() <= 1586  && $(window).width() > 1076) {
+      if ($(window).scrollTop() > 0.5) {
+        $('.el4').css({'font-size': '2.2rem', 'line-height': '2.2rem', 'padding': '5px'});
+      }
 
-      $('.nav-s').css(
-        {'display': 'none'}
-      );
+      else {
+        $('.el4').css({'font-size': '3.3rem', 'line-height': '3.3rem', 'padding': '16px'});
+      }
+
+      $('.container').css({
+        'width': '85%'});
+
+      $('.contact').css({
+        'width': '85%'});
+
+      $('.nav-s').css({
+        'display': 'none'});
 
 
-      $('.nav-left').css(
-        {'display': 'grid'}
-      );
-    
-    
-      $('.nav-right').css(
-        {'display': 'grid'}
-      );
+      $('.nav-left').css({
+        'display': 'grid'});
 
+      $('.nav-right').css({
+        'display': 'grid'});
     }
-   if ($(window).width() > 1586) {
 
-    if ($(window).scrollTop() > 0.5) {
-      $('.el4').css({'font-size': '2.9rem', 'line-height': '3rem', 'padding': '10px'});
-    }
-    else {
-      $('.el4').css({'font-size': '4.3rem', 'line-height': '4.5rem', 'padding': '16px'});
-    }
+
+
+    if ($(window).width() > 1586){
+
+      if ($(window).scrollTop() > 0.5) {
+        $('.el4').css({'font-size': '2.9rem', 'line-height': '3rem', 'padding': '10px'});
+      }
+
+      else {
+        $('.el4').css({'font-size': '4.3rem', 'line-height': '4.5rem', 'padding': '16px'});
+      }
 
       $('.container').css({'width': '85rem'});
 
-      $('.contact').css({'width': '85rem'})
+      $('.contact').css({
+        'width': '85rem'})
 
+      $('.nav-s').css({
+        'display': 'none'});
 
-      $('.nav-s').css(
-        {'display': 'none'}
-      );
+      $('.nav-left').css({
+        'display': 'grid'});
 
-      $('.nav-left').css(
-        {'display': 'grid'}
-      );
-    
-    
-      $('.nav-right').css(
-        {'display': 'grid'}
-      );
-
+      $('.nav-right').css({
+        'display': 'grid'});
     }
   });
 });
@@ -198,7 +197,6 @@ function openNavl() {
 function closeNavl() {
   document.getElementById("nav-l").style.width = "0%";
 }
-
 
 function openNavr() {
   document.getElementById("nav-r").style.width = "100%";
